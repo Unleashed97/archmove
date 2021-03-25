c51fedf8cb65c304330e7ac110e15dc344617a75
Nichkov Roman <nichkovroman@gmail.com>
2021-03-25 13:45:12 +0300

    burger menu

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,7 +1,9 @@
 const burger = ()=>{
     const burgerBtn = document.querySelector('.burger');
     const nav = document.querySelector('.nav');
+    const header = document.querySelector('.header');
     burgerBtn.addEventListener('click', ()=>{
+        header.classList.toggle('active');
         nav.classList.toggle('active');
         document.body.classList.toggle('no-scroll')
     })
